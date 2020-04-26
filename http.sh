ece2dd177960d7617690c41649a31e2a36d66832
senecaboy <58629609+senecaboy@users.noreply.github.com>
2020-04-26 04:50:23 -0400

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,3 +1,8 @@
 date >> /home/ec2-user/index.txt
 whoami >> /home/ec2-user/index.txt
+if $(rpm -aq vsftp) == ""
+then
 sudo yum install vsftpd -y
+else
+echo "vsftpd is already installed" >> /home/ec2-user/index.txt
+fi
