64f1787f708888590707b2f0ebd205d0e82a756f
senecaboy <58629609+senecaboy@users.noreply.github.com>
2020-04-26 11:50:34 -0400

    Update http.sh

--- http.sh
+++ http.sh
@@ -6,5 +6,6 @@ sudo yum install vsftpd -y
 else
 echo "vsftpd is already installed" >> /home/ec2-user/index.txt
 fi
+cd /home/ec2-user/
 ssh -i  updatedKeysysconfig.pem 3.89.58.87
 sudo yum install -y httpd
