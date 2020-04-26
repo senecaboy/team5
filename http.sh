b44a091b6b7dda4cfdd450842022b46095c599b3
senecaboy <58629609+senecaboy@users.noreply.github.com>
2020-04-26 12:21:10 -0400

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,5 +1,6 @@
-date >> /home/ec2-user/index.txt
-whoami >> /home/ec2-user/index.txt
+#date >> /home/ec2-user/index.txt
+#whoami >> /home/ec2-user/index.txt
+cd ~
 pwd > /home/ec2-user/index.txt
-ssh -i  updatedKeysysconfig.pem 10.0.2.189
-sudo yum install -y httpd
+#ssh -i  updatedKeysysconfig.pem 10.0.2.189
+#sudo yum install -y httpd
